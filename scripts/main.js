bdd8edbddc776ae5488ab2c3b3cba6a058afb347
Pol <polbene95@gmail.com>
2018-10-17 15:30:36 +0200

    Check the script

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+const raul = {
+    name: "Raul",
+    city: "Barcelona"
+};
+const mica = {
+    name: "Micaela",
+    city: "Madrid"
+};
+const pol = {
+    name: "Pol",
+    city: "Amsterdam"
+};
+const mike = {
+    name: "Mike",
+    city: "Berlin"
+}
+const roni = {
+    name: "Roni",
+    city: "Berlin"
+}
+const vasil = {
+    name: "Vasil",
+    city "Barcelona"
+}
+const mari = {
+    name: "Marí",
+    city: "Amsterdam"
+}
+const lluis = {
+    name: "Lluís",
+    city: "Barcelona"
+}
+const array = [raul,vasil,lluis,mike,roni,mari,pol,mica]
+
+console.table(array)
